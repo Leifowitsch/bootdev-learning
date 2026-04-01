29e96104d0c496f0bf24e25208a3fe032696dcc0
Leifowitsch <leifreinhold2@gmail.com>
2026-04-01 21:43:14 +0200

    Loops, lists und boolische algebra auf neustem Stand gebracht

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ print("Funktioniert")
 # git push
 
 #01.04.2026 nach 2h level 10 erreeicht grundelagen wieder aufgefrischt
+#01.04.2026 nach 4h level 15 erreeicht loops, lists und boolische algebra wieder auf neustem Stand.
+
 
