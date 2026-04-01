3601160bcbf6ad4b1622e942714cfc096bb76094
Leifowitsch <leifreinhold2@gmail.com>
2026-04-01 16:08:36 +0200

    Anleitung fürs Hochladen hinzugefügt

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Jetzt wirt coding gegrindet")
 print("Funktioniert")
+
+# Befehle um es auf Github Hochzuladen:
+
+# git add .
+
+# git commit -m "Lektion über Variablen fertig"
+
+# git push
+
+
