f6b3ff14871dd8a26ea0d2c40915531020e965e8
Leifowitsch <leifreinhold2@gmail.com>
2026-04-03 17:43:27 +0200

    Alles auf WSL umgestellt

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ print("Funktioniert")
 #01.04.2026 nach 2h level 10 ereicht grundelagen wieder aufgefrischt
 #01.04.2026 nach 4h level 15 ereeicht loops, lists und boolische algebra wieder auf neustem Stand.
 #01.04.2026 nach 7h level 17 ereicht bin bei dictionaries angekommen und habe noch mehr mit lists gearbeiet.
-
+#03.04.2026 Alles auf WSL umgestellt
 
